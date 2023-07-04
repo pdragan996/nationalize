6b258e52ec70067821f3d3c1bfdeb7e05528349b
draganpapic <pdragan96@gmail.com>
2023-07-04 21:28:40 +0200

    Name origin predictions

--- src/app/components/countries-list/countries-list.component.ts
+++ src/app/components/countries-list/countries-list.component.ts
@@ -9,21 +9,35 @@ import { GetNationalitiesService } from '../../services/get-nationalities.servic
   templateUrl: './countries-list.component.html',
   styleUrls: ['./countries-list.component.scss']
 })
-export class CountriesListComponent implements OnInit, OnDestroy{
+export class CountriesListComponent implements OnInit, OnDestroy {
 
   sub: Subscription = new Subscription();
   nationalizeData: NationalizeDataModel[] = [];
+  isDataFetchSuccess: boolean;
+  errorMessage: string;
 
   constructor(
     private _getNamesService: GetNamesService,
     private _getNationalitiesService: GetNationalitiesService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    this.sub = this._getNamesService.getNames().pipe(
-      switchMap(names => this._getNationalitiesService.getNationalities(names))
-    ).subscribe((response: NationalizeDataModel[]) => {
+    const nextFunction = (response: NationalizeDataModel[]) => {
       this.nationalizeData = response;
+      this.isDataFetchSuccess = true;
+    };
+
+    const errorFunction = err => {
+      this.isDataFetchSuccess = false;
+      this.errorMessage = err?.error?.error ?? err?.error;
+    };
+
+    this.sub = this._getNamesService.getNames().pipe(
+      switchMap((names: string[]) => this._getNationalitiesService.getNationalities(names))
+    ).subscribe({
+      next: nextFunction,
+      error: errorFunction
     });
   }
 
@@ -32,11 +46,10 @@ export class CountriesListComponent implements OnInit, OnDestroy{
   }
 
   countryTrack(index: number, item: CountryInfo): string {
-    return item.country_id
+    return item.country_id;
   }
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
-
 }
--- src/app/components/countries-list/countries-list.module.ts
+++ src/app/components/countries-list/countries-list.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { CountryNamePipe } from '../../pipes/country-name.pipe';
 import { ProbabilityPercentagePipe } from '../../pipes/probability-percentage.pipe';
 import { CountriesListComponent } from './countries-list.component';
 
 @NgModule({
-            declarations: [
-              CountriesListComponent
-            ],
-            exports: [
-              CountriesListComponent
-            ],
-            imports: [
-              CommonModule,
-              ProbabilityPercentagePipe,
-              CountryNamePipe,
-            ]
-          })
-export class CountriesListModule { }
+  declarations: [
+    CountriesListComponent
+  ],
+  exports: [
+    CountriesListComponent
+  ],
+  imports: [
+    CommonModule,
+    ProbabilityPercentagePipe,
+    CountryNamePipe
+  ]
+})
+export class CountriesListModule {
+}
